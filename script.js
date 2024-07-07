6a49b497f44e13413bad752bc552dd655d279a17
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2024-07-07 20:03:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('sidebarToggle').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar.style.display === 'block') {
+        sidebar.style.display = 'none';
+    } else {
+        sidebar.style.display = 'block';
+    }
+});
